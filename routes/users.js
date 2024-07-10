f1464af89a0c9c94ef6b953ac705ef233eceaf21
shrutivision13 <shrutinakrani.vision@gmail.com>
2024-07-10 16:27:14 +0530

    UI changes

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,10 @@ var express = require('express');
 var router = express.Router();
 const { getUsers, deleteUserById, updateUserById, createUser } = require('../controller/user');
 const verifyToken = require('../middlewear/auth');
-const { validateUser } = require('../validation/user');
 
 router.get('/getUsers', verifyToken, getUsers);
 router.put('/updateUser/:id', verifyToken, updateUserById);
 router.delete('/deleteUser/:id', verifyToken, deleteUserById);
-router.post('/createUser', verifyToken, validateUser, createUser);
+router.post('/createUser', verifyToken, createUser);
 
 module.exports = router;
